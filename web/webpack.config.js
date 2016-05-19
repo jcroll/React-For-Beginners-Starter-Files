3a4c8009164f5ffda9e549e94a5d469d35b5fdb8
Jarrett Croll <jarrett.croll@gmail.com>
2016-05-19 13:22:41 +0000

    Work

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -6,7 +6,12 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.styl$/, loader: "css-loader!stylus-loader?paths=node_modules/bootstrap-stylus/stylus/" }
+      { test: /\.styl$/, loader: "css-loader!stylus-loader?paths=node_modules/bootstrap-stylus/stylus/" },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel?presets[]=es2015"
+      }
     ]
   }
 };
